560fc39d9e4d3e3c818c8c00438450191082c0d7
AnonimPython <moscow.retro@list.ru>
2025-01-24 01:39:51 +0300

    profile page start

--- DriveOn/pages/profile.py
+++ DriveOn/pages/profile.py
@@ -3,14 +3,19 @@ from rxconfig import config
 
 from ..state import UserData
 from ..ui.colors import *
+from ..ui.navbar import navbar
 
 
 
 def profile() -> rx.Component:
     return rx.box(
         rx.mobile_only(
+            rx.box(
+                
+            ),
             rx.text(UserData.username),
             rx.text(UserData.mail),
-            color="white"
+            navbar(),
+            color="white",
         )
     )
